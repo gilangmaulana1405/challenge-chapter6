b35febcb3fe73532f62af1328f1cbfcad76777e8
Gilang <gilangmaulana541@gmail.com>
2023-05-18 12:11:41 +0700

    cleaning code

--- frontend/src/components/CariMobil.jsx
+++ frontend/src/components/CariMobil.jsx
@@ -1,5 +1,6 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
 import { Button, Card, Col, Container, Form, Nav, Offcanvas, Row } from 'react-bootstrap';
 
 import 'owl.carousel/dist/assets/owl.carousel.min.css';
